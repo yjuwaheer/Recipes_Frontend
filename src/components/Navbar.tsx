fbed453e511e93766c07f0b86dccb948ec986a99
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2023-04-07 20:20:19 -0230

    Updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <nav className="flex justify-center w-full py-3 bg-gray-100">
       <div className="w-full px-8 lg:px-0 lg:w-[900px] flex justify-between items-center">
         {/* Page */}
-        <h1 className="text-3xl font-bold">
+        <Link to="/" className="text-3xl font-bold">
           Eat<span className="text-green-600">Well</span>
-        </h1>
+        </Link>
 
         {/* Navigation */}
         <div>
